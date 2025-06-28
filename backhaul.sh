61c9a2272bc506479cb01ad2f9edc666a8632f42
Mahan <not.kup1ng@gmail.com>
2025-06-29 03:03:45 +0330

    Update backhaul.sh

--- backhaul.sh
+++ backhaul.sh
@@ -126,7 +126,7 @@ rm -rf "${config_dir}/LICENSE" >/dev/null 2>&1
 rm -rf "${config_dir}/README.md" >/dev/null 2>&1
 }
 download_and_extract_backhaul
-SERVER_IP=$(hostname -I | awk '\''{print $1}'\'')
+SERVER_IP=$(hostname -I | awk '{print $1}')
 check_config_backup() {
 missing_services=()
 for config in "${config_dir}"/iran*.toml "${config_dir}"/kharej*.toml; do
@@ -161,7 +161,7 @@ service_file="${entry%%:*}"
 location="${entry#*:}"; location="${location%%:*}"
 tunnel_port="${entry##*:}"
 config_file="${config_dir}/${location}${tunnel_port}.toml"
-desc_loc="$(tr '\''[:lower:]'\'' '\''[:upper:]'\'' <<< ${location:0:1})${location:1}"
+desc_loc="$(tr '[:lower:]' '[:upper:]' <<< ${location:0:1})${location:1}"
 cat > "$service_file" <<EOF
 [Unit]
 Description=Backhaul $desc_loc Port $tunnel_port
@@ -188,11 +188,12 @@ check_config_backup
 display_logo() {
 echo -e "${CYAN}"
 cat << "EOF"
-▗▄▄▖   ▗▄▄▖ ▗▄▄▄▖ ▗▄▄▖  ▗▄▄▖ ▗▄▄▄▖ ▗▄▖ ▗▄▄▖
-▐▌ ▐▌ ▐▌   ▐▌    ▐▌ ▐▌ ▐▌ ▐▌   ▐▌  ▐▌ ▐▌▐▌ ▐▌
-▐▛▀▘  ▐▌ ▀▖▐▛▀▘  ▐▛▀▘  ▐▛▀▘   ▐▌  ▐▛▀▘ ▐▛▀▘
-▐▌    ▝▙▄▞▘▐▌    ▐▌    ▐▌    ▗▛▖  ▐▌   ▐▌
-▝▘     ▝▀▘ ▝▘    ▝▘    ▝▘   ▝▀▀▘  ▝▘   ▝▘
+  _________________   ________   .____        _____    ____  __. ___ ___  
+ /   _____/\_____  \  \_____  \  |    |      /  _  \  |    |/ _|/   |   \ 
+ \_____  \  /   |   \  /   |   \ |    |     /  /_\  \ |      < /    ~    \
+ /        \/    |    \/    |    \|    |___ /    |    \|    |  \\    Y    /
+/_______  /\_______  /\_______  /|_______ \\____|__  /|____|__ \\___|_  / 
+        \/         \/         \/         \/        \/         \/      \/  
 Lightning-fast reverse tunneling solution
 EOF
 echo -e "${NC}${GREEN}"
@@ -252,7 +253,7 @@ fi
 }
 configure_tunnel() {
 if [[ ! -d "$config_dir" ]]; then
-echo -e "\n${RED}Backhaul-Core directory not found. Install it first through '\''Install Backhaul core'\'' option.${NC}\n"
+echo -e "\n${RED}Backhaul-Core directory not found. Install it first through 'Install Backhaul core' option.${NC}\n"
 read -p "Press Enter to continue..."
 return 1
 fi
@@ -325,9 +326,9 @@ if [[ -z "$tun_subnet" ]]; then
 tun_subnet="10.10.10.0/24"
 fi
 if [[ "$tun_subnet" =~ ^[0-9]{1,3}(\.[0-9]{1,3}){3}/[0-9]{1,2}$ ]]; then
-IFS='\''/'\'' read -r ip subnet <<< "$tun_subnet"
+IFS='/' read -r ip subnet <<< "$tun_subnet"
 if [[ "$subnet" -le 32 && "$subnet" -ge 1 ]]; then
-IFS='\''.'\'' read -r a b c d <<< "$ip"
+IFS='.' read -r a b c d <<< "$ip"
 if [[ "$a" -le 255 && "$b" -le 255 && "$c" -le 255 && "$d" -le 255 ]]; then
 echo
 break
@@ -362,7 +363,7 @@ if [[ -z "$accept_udp" ]]; then
 accept_udp="false"
 fi
 if [[ "$accept_udp" != "true" && "$accept_udp" != "false" ]]; then
-colorize red "Invalid input. Please enter '\''true'\'' or '\''false'\''."
+colorize red "Invalid input. Please enter 'true' or 'false'."
 echo
 fi
 done
@@ -398,7 +399,7 @@ if [[ -z "$nodelay" ]]; then
 nodelay=true
 fi
 if [[ "$nodelay" != "true" && "$nodelay" != "false" ]]; then
-colorize red "Invalid input. Please enter '\''true'\'' or '\''false'\''."
+colorize red "Invalid input. Please enter 'true' or 'false'."
 echo
 fi
 done
@@ -469,7 +470,7 @@ if [[ -z "$sniffer" ]]; then
 sniffer=false
 fi
 if [[ "$sniffer" != "true" && "$sniffer" != "false" ]]; then
-colorize red "Invalid input. Please enter '\''true'\'' or '\''false'\''."
+colorize red "Invalid input. Please enter 'true' or 'false'."
 echo
 fi
 done
@@ -505,7 +506,7 @@ if [[ -z "$proxy_protocol" ]]; then
 proxy_protocol=false
 fi
 if [[ "$proxy_protocol" != "true" && "$proxy_protocol" != "false" ]]; then
-colorize red "Invalid input. Please enter '\''true'\'' or '\''false'\''."
+colorize red "Invalid input. Please enter 'true' or 'false'."
 echo
 fi
 done
@@ -525,8 +526,8 @@ echo "7. 443=1.1.1.1:5201         - Listen on local port 443 and forward to a sp
 echo ""
 echo -ne "[*] Enter your ports in the specified formats (separated by commas): "
 read -r input_ports
-input_ports=$(echo "$input_ports" | tr -d '\'' '\'')
-IFS='\'','\'' read -r -a ports <<< "$input_ports"
+input_ports=$(echo "$input_ports" | tr -d ' ')
+IFS=',' read -r -a ports <<< "$input_ports"
 fi
 cat << EOF > "${config_dir}/iran${tunnel_port}.toml"
 [server]
@@ -663,9 +664,9 @@ if [[ -z "$tun_subnet" ]]; then
 tun_subnet="10.10.10.0/24"
 fi
 if [[ "$tun_subnet" =~ ^[0-9]{1,3}(\.[0-9]{1,3}){3}/[0-9]{1,2}$ ]]; then
-IFS='\''/'\'' read -r ip subnet <<< "$tun_subnet"
+IFS='/' read -r ip subnet <<< "$tun_subnet"
 if [[ "$subnet" -le 32 && "$subnet" -ge 1 ]]; then
-IFS='\''.'\'' read -r a b c d <<< "$ip"
+IFS='.' read -r a b c d <<< "$ip"
 if [[ "$a" -le 255 && "$b" -le 255 && "$c" -le 255 && "$d" -le 255 ]]; then
 echo
 break
@@ -722,7 +723,7 @@ if [[ -z "$nodelay" ]]; then
 nodelay=true
 fi
 if [[ "$nodelay" != "true" && "$nodelay" != "false" ]]; then
-colorize red "Invalid input. Please enter '\''true'\'' or '\''false'\''."
+colorize red "Invalid input. Please enter 'true' or 'false'."
 echo
 fi
 done
@@ -771,7 +772,7 @@ if [[ -z "$sniffer" ]]; then
 sniffer=false
 fi
 if [[ "$sniffer" != "true" && "$sniffer" != "false" ]]; then
-colorize red "Invalid input. Please enter '\''true'\'' or '\''false'\''."
+colorize red "Invalid input. Please enter 'true' or 'false'."
 echo
 fi
 done
@@ -807,7 +808,7 @@ if [[ -z "$ip_limit" ]]; then
 ip_limit=false
 fi
 if [[ "$ip_limit" != "true" && "$ip_limit" != "false" ]]; then
-colorize red "Invalid input. Please enter '\''true'\'' or '\''false'\''."
+colorize red "Invalid input. Please enter 'true' or 'false'."
 echo
 fi
 done
@@ -1053,12 +1054,12 @@ echo -ne "${YELLOW}Reboot now? (Recommended) (y/n): ${NC}"
 while true; do
 read choice
 echo
-if [[ "$choice" == '\''y'\'' || "$choice" == '\''Y'\'' ]]; then
+if [[ "$choice" == 'y' || "$choice" == 'Y' ]]; then
 sleep 0.5
 reboot
 exit 0
 fi
-if [[ "$choice" == '\''n'\'' || "$choice" == '\''N'\'' ]]; then
+if [[ "$choice" == 'n' || "$choice" == 'N' ]]; then
 break
 fi
 done
@@ -1073,61 +1074,61 @@ echo
 echo -e  "${YELLOW}Optimizing the Network...${NC}"
 echo
 sleep 0.5
-sed -i -e '\''/fs.file-max/d'\'' \
--e '\''/net.core.default_qdisc/d'\'' \
--e '\''/net.core.netdev_max_backlog/d'\'' \
--e '\''/net.core.optmem_max/d'\'' \
--e '\''/net.core.somaxconn/d'\'' \
--e '\''/net.core.rmem_max/d'\'' \
--e '\''/net.core.wmem_max/d'\'' \
--e '\''/net.core.rmem_default/d'\'' \
--e '\''/net.core.wmem_default/d'\'' \
--e '\''/net.ipv4.tcp_rmem/d'\'' \
--e '\''/net.ipv4.tcp_wmem/d'\'' \
--e '\''/net.ipv4.tcp_congestion_control/d'\'' \
--e '\''/net.ipv4.tcp_fastopen/d'\'' \
--e '\''/net.ipv4.tcp_fin_timeout/d'\'' \
--e '\''/net.ipv4.tcp_keepalive_time/d'\'' \
--e '\''/net.ipv4.tcp_keepalive_probes/d'\'' \
--e '\''/net.ipv4.tcp_keepalive_intvl/d'\'' \
--e '\''/net.ipv4.tcp_max_orphans/d'\'' \
--e '\''/net.ipv4.tcp_max_syn_backlog/d'\'' \
--e '\''/net.ipv4.tcp_max_tw_buckets/d'\'' \
--e '\''/net.ipv4.tcp_mem/d'\'' \
--e '\''/net.ipv4.tcp_mtu_probing/d'\'' \
--e '\''/net.ipv4.tcp_notsent_lowat/d'\'' \
--e '\''/net.ipv4.tcp_retries2/d'\'' \
--e '\''/net.ipv4.tcp_sack/d'\'' \
--e '\''/net.ipv4.tcp_dsack/d'\'' \
--e '\''/net.ipv4.tcp_slow_start_after_idle/d'\'' \
--e '\''/net.ipv4.tcp_window_scaling/d'\'' \
--e '\''/net.ipv4.tcp_adv_win_scale/d'\'' \
--e '\''/net.ipv4.tcp_ecn/d'\'' \
--e '\''/net.ipv4.tcp_ecn_fallback/d'\'' \
--e '\''/net.ipv4.tcp_syncookies/d'\'' \
--e '\''/net.ipv4.udp_mem/d'\'' \
--e '\''/net.ipv6.conf.all.disable_ipv6/d'\'' \
--e '\''/net.ipv6.conf.default.disable_ipv6/d'\'' \
--e '\''/net.ipv6.conf.lo.disable_ipv6/d'\'' \
--e '\''/net.unix.max_dgram_qlen/d'\'' \
--e '\''/vm.min_free_kbytes/d'\'' \
--e '\''/vm.swappiness/d'\'' \
--e '\''/vm.vfs_cache_pressure/d'\'' \
--e '\''/net.ipv4.conf.default.rp_filter/d'\'' \
--e '\''/net.ipv4.conf.all.rp_filter/d'\'' \
--e '\''/net.ipv4.conf.all.accept_source_route/d'\'' \
--e '\''/net.ipv4.conf.default.accept_source_route/d'\'' \
--e '\''/net.ipv4.neigh.default.gc_thresh1/d'\'' \
--e '\''/net.ipv4.neigh.default.gc_thresh2/d'\'' \
--e '\''/net.ipv4.neigh.default.gc_thresh3/d'\'' \
--e '\''/net.ipv4.neigh.default.gc_stale_time/d'\'' \
--e '\''/net.ipv4.conf.default.arp_announce/d'\'' \
--e '\''/net.ipv4.conf.lo.arp_announce/d'\'' \
--e '\''/net.ipv4.conf.all.arp_announce/d'\'' \
--e '\''/kernel.panic/d'\'' \
--e '\''/vm.dirty_ratio/d'\'' \
--e '\''/^#/d'\'' \
--e '\''/^$/d'\'' \
+sed -i -e '/fs.file-max/d' \
+-e '/net.core.default_qdisc/d' \
+-e '/net.core.netdev_max_backlog/d' \
+-e '/net.core.optmem_max/d' \
+-e '/net.core.somaxconn/d' \
+-e '/net.core.rmem_max/d' \
+-e '/net.core.wmem_max/d' \
+-e '/net.core.rmem_default/d' \
+-e '/net.core.wmem_default/d' \
+-e '/net.ipv4.tcp_rmem/d' \
+-e '/net.ipv4.tcp_wmem/d' \
+-e '/net.ipv4.tcp_congestion_control/d' \
+-e '/net.ipv4.tcp_fastopen/d' \
+-e '/net.ipv4.tcp_fin_timeout/d' \
+-e '/net.ipv4.tcp_keepalive_time/d' \
+-e '/net.ipv4.tcp_keepalive_probes/d' \
+-e '/net.ipv4.tcp_keepalive_intvl/d' \
+-e '/net.ipv4.tcp_max_orphans/d' \
+-e '/net.ipv4.tcp_max_syn_backlog/d' \
+-e '/net.ipv4.tcp_max_tw_buckets/d' \
+-e '/net.ipv4.tcp_mem/d' \
+-e '/net.ipv4.tcp_mtu_probing/d' \
+-e '/net.ipv4.tcp_notsent_lowat/d' \
+-e '/net.ipv4.tcp_retries2/d' \
+-e '/net.ipv4.tcp_sack/d' \
+-e '/net.ipv4.tcp_dsack/d' \
+-e '/net.ipv4.tcp_slow_start_after_idle/d' \
+-e '/net.ipv4.tcp_window_scaling/d' \
+-e '/net.ipv4.tcp_adv_win_scale/d' \
+-e '/net.ipv4.tcp_ecn/d' \
+-e '/net.ipv4.tcp_ecn_fallback/d' \
+-e '/net.ipv4.tcp_syncookies/d' \
+-e '/net.ipv4.udp_mem/d' \
+-e '/net.ipv6.conf.all.disable_ipv6/d' \
+-e '/net.ipv6.conf.default.disable_ipv6/d' \
+-e '/net.ipv6.conf.lo.disable_ipv6/d' \
+-e '/net.unix.max_dgram_qlen/d' \
+-e '/vm.min_free_kbytes/d' \
+-e '/vm.swappiness/d' \
+-e '/vm.vfs_cache_pressure/d' \
+-e '/net.ipv4.conf.default.rp_filter/d' \
+-e '/net.ipv4.conf.all.rp_filter/d' \
+-e '/net.ipv4.conf.all.accept_source_route/d' \
+-e '/net.ipv4.conf.default.accept_source_route/d' \
+-e '/net.ipv4.neigh.default.gc_thresh1/d' \
+-e '/net.ipv4.neigh.default.gc_thresh2/d' \
+-e '/net.ipv4.neigh.default.gc_thresh3/d' \
+-e '/net.ipv4.neigh.default.gc_stale_time/d' \
+-e '/net.ipv4.conf.default.arp_announce/d' \
+-e '/net.ipv4.conf.lo.arp_announce/d' \
+-e '/net.ipv4.conf.all.arp_announce/d' \
+-e '/kernel.panic/d' \
+-e '/vm.dirty_ratio/d' \
+-e '/^#/d' \
+-e '/^$/d' \
 "$SYS_PATH"
 cat <<EOF >> "$SYS_PATH"
 fs.file-max = 67108864
@@ -1194,20 +1195,20 @@ echo
 echo -e "${YELLOW}Optimizing System Limits...${NC}"
 echo
 sleep 0.5
-sed -i '\''/ulimit -c/d'\'' $PROF_PATH
-sed -i '\''/ulimit -d/d'\'' $PROF_PATH
-sed -i '\''/ulimit -f/d'\'' $PROF_PATH
-sed -i '\''/ulimit -i/d'\'' $PROF_PATH
-sed -i '\''/ulimit -l/d'\'' $PROF_PATH
-sed -i '\''/ulimit -m/d'\'' $PROF_PATH
-sed -i '\''/ulimit -n/d'\'' $PROF_PATH
-sed -i '\''/ulimit -q/d'\'' $PROF_PATH
-sed -i '\''/ulimit -s/d'\'' $PROF_PATH
-sed -i '\''/ulimit -t/d'\'' $PROF_PATH
-sed -i '\''/ulimit -u/d'\'' $PROF_PATH
-sed -i '\''/ulimit -v/d'\'' $PROF_PATH
-sed -i '\''/ulimit -x/d'\'' $PROF_PATH
-sed -i '\''/ulimit -s/d'\'' $PROF_PATH
+sed -i '/ulimit -c/d' $PROF_PATH
+sed -i '/ulimit -d/d' $PROF_PATH
+sed -i '/ulimit -f/d' $PROF_PATH
+sed -i '/ulimit -i/d' $PROF_PATH
+sed -i '/ulimit -l/d' $PROF_PATH
+sed -i '/ulimit -m/d' $PROF_PATH
+sed -i '/ulimit -n/d' $PROF_PATH
+sed -i '/ulimit -q/d' $PROF_PATH
+sed -i '/ulimit -s/d' $PROF_PATH
+sed -i '/ulimit -t/d' $PROF_PATH
+sed -i '/ulimit -u/d' $PROF_PATH
+sed -i '/ulimit -v/d' $PROF_PATH
+sed -i '/ulimit -x/d' $PROF_PATH
+sed -i '/ulimit -s/d' $PROF_PATH
 echo "ulimit -c unlimited" | tee -a $PROF_PATH
 echo "ulimit -d unlimited" | tee -a $PROF_PATH
 echo "ulimit -f unlimited" | tee -a $PROF_PATH
@@ -1296,7 +1297,7 @@ curl -s -L -o "$DEST_DIR/$BACKHAUL_SCRIPT" "$SCRIPT_URL"
 echo
 if [ $? -eq 0 ]; then
 chmod +x "$DEST_DIR/$BACKHAUL_SCRIPT"
-colorize yellow "Type '\''backhaul'\'' to run the script.\n" bold
+colorize yellow "Type 'backhaul' to run the script.\n" bold
 colorize yellow "For removing script type: rm -rf /usr/bin/backhaul\n" bold
 press_key
 exit 0
@@ -1306,12 +1307,12 @@ sleep 1
 return 1
 fi
 }
-RED='\''\033[0;31m'\''
-GREEN='\''\033[0;32m'\''
-YELLOW='\''\033[0;33m'\''
-CYAN='\''\e[36m'\''
-MAGENTA="\e[95m"
-NC='\''\033[0m'\'' # No Color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+CYAN='\033[0;36m'
+MAGENTA='\033[0;35m'
+NC='\033[0m'  # No Color
 display_menu() {
 clear
 display_logo
