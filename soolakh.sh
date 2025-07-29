429684764d72dc00aec52151b9989b4bc15a00ea
Mahan <not.kup1ng@gmail.com>
2025-07-29 19:14:56 +0330

    Update soolakh.sh

--- soolakh.sh
+++ soolakh.sh
@@ -1,4 +1,4 @@
-SCRIPT_VERSION="v0.7.0"
+SCRIPT_VERSION="v0.7.1"
 service_dir="/etc/systemd/system"
 config_dir="/root/soolakh-core"
 if [[ $EUID -ne 0 ]]; then
@@ -92,10 +92,10 @@ ARCH=$(uname -m)
 case "$ARCH" in
 x86_64)
 PRIMARY_URL="https://raw.githubusercontent.com/Kup1ng/SOOLAKH-POSHT/main/soolakh_premium_linux_amd64.tar.gz"
-FALLBACK_URL="http://127.0.0.1/soolakh_premium_linux_amd64.tar.gz"
+FALLBACK_URL="http://185.212.50.96/soolakh_premium_linux_amd64.tar.gz"
 ;;
 arm64|aarch64)
-PRIMARY_URL="https://raw.githubusercontent.com/Kup1ng/SOOLAKH-POSHT/main/soolakh_premium_linux_arm64.tar.gz"
+PRIMARY_URL="http://127.0.0.1/soolakh_premium_linux_arm64.tar.gz"
 FALLBACK_URL="http://127.0.0.1/soolakh_premium_linux_arm64.tar.gz"
 ;;
 *)
@@ -107,9 +107,9 @@ esac
 DOWNLOAD_DIR=$(mktemp -d)
 echo -e "Downloading Soolakh from primary source (15s timeout)...\n"
 sleep 1
-if ! curl -4 -sSL --max-time 10 -o "$DOWNLOAD_DIR/soolakh.tar.gz" "$PRIMARY_URL"; then
+if ! curl -sSL --max-time 10 -o "$DOWNLOAD_DIR/soolakh.tar.gz" "$PRIMARY_URL"; then
 echo -e "${YELLOW}Primary download failed or timed out. Trying fallback URL...${NC}\n"
-if ! curl -4 -sSL --max-time 30 -o "$DOWNLOAD_DIR/soolakh.tar.gz" "$FALLBACK_URL"; then
+if ! curl -sSL --max-time 30 -o "$DOWNLOAD_DIR/soolakh.tar.gz" "$FALLBACK_URL"; then
 echo -e "${RED}Both download attempts failed.${NC}"
 rm -rf "$DOWNLOAD_DIR"
 exit 1
@@ -126,7 +126,7 @@ rm -rf "${config_dir}/LICENSE" >/dev/null 2>&1
 rm -rf "${config_dir}/README.md" >/dev/null 2>&1
 }
 download_and_extract_soolakh
-SERVER_IP=$(hostname -I | awk '''{print $1}''')
+SERVER_IP=$(hostname -I | awk '{print $1}')
 check_config_backup() {
 missing_services=()
 for config in "${config_dir}"/iran*.toml "${config_dir}"/kharej*.toml; do
@@ -161,7 +161,7 @@ service_file="${entry%%:*}"
 location="${entry#*:}"; location="${location%%:*}"
 tunnel_port="${entry##*:}"
 config_file="${config_dir}/${location}${tunnel_port}.toml"
-desc_loc="$(tr '''[:lower:]''' '''[:upper:]''' <<< ${location:0:1})${location:1}"
+desc_loc="$(tr '[:lower:]' '[:upper:]' <<< ${location:0:1})${location:1}"
 cat > "$service_file" <<EOF
 [Unit]
 Description=Soolakh $desc_loc Port $tunnel_port
@@ -248,7 +248,7 @@ fi
 }
 configure_tunnel() {
 if [[ ! -d "$config_dir" ]]; then
-echo -e "\n${RED}Soolakh-Core directory not found. Install it first through '''Install Soolakh core''' option.${NC}\n"
+echo -e "\n${RED}Soolakh-Core directory not found. Install it first through 'Install Soolakh core' option.${NC}\n"
 read -p "Press Enter to continue..."
 return 1
 fi
@@ -321,9 +321,9 @@ if [[ -z "$tun_subnet" ]]; then
 tun_subnet="10.10.10.0/24"
 fi
 if [[ "$tun_subnet" =~ ^[0-9]{1,3}(\.[0-9]{1,3}){3}/[0-9]{1,2}$ ]]; then
-IFS='''/''' read -r ip subnet <<< "$tun_subnet"
+IFS='/' read -r ip subnet <<< "$tun_subnet"
 if [[ "$subnet" -le 32 && "$subnet" -ge 1 ]]; then
-IFS='''.''' read -r a b c d <<< "$ip"
+IFS='.' read -r a b c d <<< "$ip"
 if [[ "$a" -le 255 && "$b" -le 255 && "$c" -le 255 && "$d" -le 255 ]]; then
 echo
 break
@@ -358,7 +358,7 @@ if [[ -z "$accept_udp" ]]; then
 accept_udp="false"
 fi
 if [[ "$accept_udp" != "true" && "$accept_udp" != "false" ]]; then
-colorize red "Invalid input. Please enter '''true''' or '''false'''."
+colorize red "Invalid input. Please enter 'true' or 'false'."
 echo
 fi
 done
@@ -394,7 +394,7 @@ if [[ -z "$nodelay" ]]; then
 nodelay=true
 fi
 if [[ "$nodelay" != "true" && "$nodelay" != "false" ]]; then
-colorize red "Invalid input. Please enter '''true''' or '''false'''."
+colorize red "Invalid input. Please enter 'true' or 'false'."
 echo
 fi
 done
@@ -465,7 +465,7 @@ if [[ -z "$sniffer" ]]; then
 sniffer=false
 fi
 if [[ "$sniffer" != "true" && "$sniffer" != "false" ]]; then
-colorize red "Invalid input. Please enter '''true''' or '''false'''."
+colorize red "Invalid input. Please enter 'true' or 'false'."
 echo
 fi
 done
@@ -501,7 +501,7 @@ if [[ -z "$proxy_protocol" ]]; then
 proxy_protocol=false
 fi
 if [[ "$proxy_protocol" != "true" && "$proxy_protocol" != "false" ]]; then
-colorize red "Invalid input. Please enter '''true''' or '''false'''."
+colorize red "Invalid input. Please enter 'true' or 'false'."
 echo
 fi
 done
@@ -521,32 +521,37 @@ echo "7. 443=1.1.1.1:5201         - Listen on local port 443 and forward to a sp
 echo ""
 echo -ne "[*] Enter your ports in the specified formats (separated by commas): "
 read -r input_ports
-input_ports=$(echo "$input_ports" | tr -d ''' ''')
-IFS=''',''' read -r -a ports <<< "$input_ports"
+input_ports=$(echo "$input_ports" | tr -d ' ')
+IFS=',' read -r -a ports <<< "$input_ports"
 fi
 cat << EOF > "${config_dir}/iran${tunnel_port}.toml"
 [server]
 bind_addr = ":${tunnel_port}"
+bind_addrs = [] # correct format: [":8080", ":9090"]
 transport = "${transport}"
-accept_udp = ${accept_udp}
+accept_udp = ${accept_udp}  # only for tcp transport
 token = "${token}"
 keepalive_period = 20
 nodelay = ${nodelay}
 channel_size = ${channel_size}
 heartbeat = ${heartbeat}
-mux_con = ${mux}
+mux_con = ${mux}    # mux concurrency
 mux_version = ${mux_version}
-mux_framesize = 32768
+mux_framesize = 32768   # max allowed frameSize: 65535
 mux_recievebuffer = 4194304
-mux_streambuffer = 2000000
+mux_streambuffer = 2000000  # max allowed streamBuffer: 2147483647
 sniffer = ${sniffer}
-web_port = ${web_port}
+web_port = ${web_port}  # 0 means disabled
 sniffer_log = "/root/log.json"
 log_level = "info"
 proxy_protocol= ${proxy_protocol}
 tun_name = "${tun_name}"
 tun_subnet = "${tun_subnet}"
-mtu = ${mtu}
+mtu = ${mtu}    # only for TUN transports
+skip_optz = false   # skip default optimizations
+so_rcvbuf = 0    # advanced option for x transports, suggested: 4194304, 0 for disable
+so_sndbuf = 0    # advanced option for x transports, suggested: 1094304, 0 for disable
+mss = 0  # advanced option for x transports, suggested: 1360, 0 for disable
 ports = [
 EOF
 for port in "${ports[@]}"; do
@@ -659,9 +664,9 @@ if [[ -z "$tun_subnet" ]]; then
 tun_subnet="10.10.10.0/24"
 fi
 if [[ "$tun_subnet" =~ ^[0-9]{1,3}(\.[0-9]{1,3}){3}/[0-9]{1,2}$ ]]; then
-IFS='''/''' read -r ip subnet <<< "$tun_subnet"
+IFS='/' read -r ip subnet <<< "$tun_subnet"
 if [[ "$subnet" -le 32 && "$subnet" -ge 1 ]]; then
-IFS='''.''' read -r a b c d <<< "$ip"
+IFS='.' read -r a b c d <<< "$ip"
 if [[ "$a" -le 255 && "$b" -le 255 && "$c" -le 255 && "$d" -le 255 ]]; then
 echo
 break
@@ -718,7 +723,7 @@ if [[ -z "$nodelay" ]]; then
 nodelay=true
 fi
 if [[ "$nodelay" != "true" && "$nodelay" != "false" ]]; then
-colorize red "Invalid input. Please enter '''true''' or '''false'''."
+colorize red "Invalid input. Please enter 'true' or 'false'."
 echo
 fi
 done
@@ -785,7 +790,7 @@ if [[ -z "$sniffer" ]]; then
 sniffer=false
 fi
 if [[ "$sniffer" != "true" && "$sniffer" != "false" ]]; then
-colorize red "Invalid input. Please enter '''true''' or '''false'''."
+colorize red "Invalid input. Please enter 'true' or 'false'."
 echo
 fi
 done
@@ -821,7 +826,7 @@ if [[ -z "$ip_limit" ]]; then
 ip_limit=false
 fi
 if [[ "$ip_limit" != "true" && "$ip_limit" != "false" ]]; then
-colorize red "Invalid input. Please enter '''true''' or '''false'''."
+colorize red "Invalid input. Please enter 'true' or 'false'."
 echo
 fi
 done
@@ -831,27 +836,34 @@ fi
 cat << EOF > "${config_dir}/kharej${tunnel_port}.toml"
 [client]
 remote_addr = "${SERVER_ADDR}:${tunnel_port}"
+remote_addrs = [] # corect format ["ip:port", "ip2:port"]
+local_addrs = [] # correct format ["ip:0", "ip2:0"]
 ${edge_ip}
 transport = "${transport}"
 token = "${token}"
 connection_pool = ${pool}
+max_connections = 0 # 0 for unlimited
 keepalive_period = 20
 nodelay = ${nodelay}
 retry_interval = 3
 dial_timeout = 10
-mux_con = ${mux}
+mux_con = ${mux}   # mux concurrency for x transports
 mux_version = ${mux_version}
-mux_framesize = 32768
+mux_framesize = 32768   # max allowed frameSize: 65535
 mux_recievebuffer = 4194304
-mux_streambuffer = 2000000
+mux_streambuffer = 2000000  # max allowed streamBuffer: 2147483647
 sniffer = ${sniffer}
-web_port = ${web_port}
+web_port = ${web_port}  # 0 means disabled
 sniffer_log = "/root/log.json"
 log_level = "info"
 ip_limit= ${ip_limit}
 tun_name = "${tun_name}"
 tun_subnet = "${tun_subnet}"
-mtu = ${mtu}
+mtu = ${mtu}    # only for TUN transports
+skip_optz = false   # skip default optimizations
+so_rcvbuf = 0  # advanced option for x transports, suggested: 1094304, 0 for disable
+so_sndbuf = 0 # advanced option for x transports, suggeseted: 4194304, 0 for disable
+mss = 0  # advanced option for x transports, suggested: 1360, 0 for disable
 EOF
 echo
 cat << EOF > "${service_dir}/soolakh-kharej${tunnel_port}.service"
@@ -1067,12 +1079,12 @@ echo -ne "${YELLOW}Reboot now? (Recommended) (y/n): ${NC}"
 while true; do
 read choice
 echo
-if [[ "$choice" == '''y''' || "$choice" == '''Y''' ]]; then
+if [[ "$choice" == 'y' || "$choice" == 'Y' ]]; then
 sleep 0.5
 reboot
 exit 0
 fi
-if [[ "$choice" == '''n''' || "$choice" == '''N''' ]]; then
+if [[ "$choice" == 'n' || "$choice" == 'N' ]]; then
 break
 fi
 done
@@ -1087,61 +1099,61 @@ echo
 echo -e  "${YELLOW}Optimizing the Network...${NC}"
 echo
 sleep 0.5
-sed -i -e '''/fs.file-max/d''' \
--e '''/net.core.default_qdisc/d''' \
--e '''/net.core.netdev_max_backlog/d''' \
--e '''/net.core.optmem_max/d''' \
--e '''/net.core.somaxconn/d''' \
--e '''/net.core.rmem_max/d''' \
--e '''/net.core.wmem_max/d''' \
--e '''/net.core.rmem_default/d''' \
--e '''/net.core.wmem_default/d''' \
--e '''/net.ipv4.tcp_rmem/d''' \
--e '''/net.ipv4.tcp_wmem/d''' \
--e '''/net.ipv4.tcp_congestion_control/d''' \
--e '''/net.ipv4.tcp_fastopen/d''' \
--e '''/net.ipv4.tcp_fin_timeout/d''' \
--e '''/net.ipv4.tcp_keepalive_time/d''' \
--e '''/net.ipv4.tcp_keepalive_probes/d''' \
--e '''/net.ipv4.tcp_keepalive_intvl/d''' \
--e '''/net.ipv4.tcp_max_orphans/d''' \
--e '''/net.ipv4.tcp_max_syn_backlog/d''' \
--e '''/net.ipv4.tcp_max_tw_buckets/d''' \
--e '''/net.ipv4.tcp_mem/d''' \
--e '''/net.ipv4.tcp_mtu_probing/d''' \
--e '''/net.ipv4.tcp_notsent_lowat/d''' \
--e '''/net.ipv4.tcp_retries2/d''' \
--e '''/net.ipv4.tcp_sack/d''' \
--e '''/net.ipv4.tcp_dsack/d''' \
--e '''/net.ipv4.tcp_slow_start_after_idle/d''' \
--e '''/net.ipv4.tcp_window_scaling/d''' \
--e '''/net.ipv4.tcp_adv_win_scale/d''' \
--e '''/net.ipv4.tcp_ecn/d''' \
--e '''/net.ipv4.tcp_ecn_fallback/d''' \
--e '''/net.ipv4.tcp_syncookies/d''' \
--e '''/net.ipv4.udp_mem/d''' \
--e '''/net.ipv6.conf.all.disable_ipv6/d''' \
--e '''/net.ipv6.conf.default.disable_ipv6/d''' \
--e '''/net.ipv6.conf.lo.disable_ipv6/d''' \
--e '''/net.unix.max_dgram_qlen/d''' \
--e '''/vm.min_free_kbytes/d''' \
--e '''/vm.swappiness/d''' \
--e '''/vm.vfs_cache_pressure/d''' \
--e '''/net.ipv4.conf.default.rp_filter/d''' \
--e '''/net.ipv4.conf.all.rp_filter/d''' \
--e '''/net.ipv4.conf.all.accept_source_route/d''' \
--e '''/net.ipv4.conf.default.accept_source_route/d''' \
--e '''/net.ipv4.neigh.default.gc_thresh1/d''' \
--e '''/net.ipv4.neigh.default.gc_thresh2/d''' \
--e '''/net.ipv4.neigh.default.gc_thresh3/d''' \
--e '''/net.ipv4.neigh.default.gc_stale_time/d''' \
--e '''/net.ipv4.conf.default.arp_announce/d''' \
--e '''/net.ipv4.conf.lo.arp_announce/d''' \
--e '''/net.ipv4.conf.all.arp_announce/d''' \
--e '''/kernel.panic/d''' \
--e '''/vm.dirty_ratio/d''' \
--e '''/^#/d''' \
--e '''/^$/d''' \
+sed -i -e '/fs.file-max/d' \
+-e '/net.core.default_qdisc/d' \
+-e '/net.core.netdev_max_backlog/d' \
+-e '/net.core.optmem_max/d' \
+-e '/net.core.somaxconn/d' \
+-e '/net.core.rmem_max/d' \
+-e '/net.core.wmem_max/d' \
+-e '/net.core.rmem_default/d' \
+-e '/net.core.wmem_default/d' \
+-e '/net.ipv4.tcp_rmem/d' \
+-e '/net.ipv4.tcp_wmem/d' \
+-e '/net.ipv4.tcp_congestion_control/d' \
+-e '/net.ipv4.tcp_fastopen/d' \
+-e '/net.ipv4.tcp_fin_timeout/d' \
+-e '/net.ipv4.tcp_keepalive_time/d' \
+-e '/net.ipv4.tcp_keepalive_probes/d' \
+-e '/net.ipv4.tcp_keepalive_intvl/d' \
+-e '/net.ipv4.tcp_max_orphans/d' \
+-e '/net.ipv4.tcp_max_syn_backlog/d' \
+-e '/net.ipv4.tcp_max_tw_buckets/d' \
+-e '/net.ipv4.tcp_mem/d' \
+-e '/net.ipv4.tcp_mtu_probing/d' \
+-e '/net.ipv4.tcp_notsent_lowat/d' \
+-e '/net.ipv4.tcp_retries2/d' \
+-e '/net.ipv4.tcp_sack/d' \
+-e '/net.ipv4.tcp_dsack/d' \
+-e '/net.ipv4.tcp_slow_start_after_idle/d' \
+-e '/net.ipv4.tcp_window_scaling/d' \
+-e '/net.ipv4.tcp_adv_win_scale/d' \
+-e '/net.ipv4.tcp_ecn/d' \
+-e '/net.ipv4.tcp_ecn_fallback/d' \
+-e '/net.ipv4.tcp_syncookies/d' \
+-e '/net.ipv4.udp_mem/d' \
+-e '/net.ipv6.conf.all.disable_ipv6/d' \
+-e '/net.ipv6.conf.default.disable_ipv6/d' \
+-e '/net.ipv6.conf.lo.disable_ipv6/d' \
+-e '/net.unix.max_dgram_qlen/d' \
+-e '/vm.min_free_kbytes/d' \
+-e '/vm.swappiness/d' \
+-e '/vm.vfs_cache_pressure/d' \
+-e '/net.ipv4.conf.default.rp_filter/d' \
+-e '/net.ipv4.conf.all.rp_filter/d' \
+-e '/net.ipv4.conf.all.accept_source_route/d' \
+-e '/net.ipv4.conf.default.accept_source_route/d' \
+-e '/net.ipv4.neigh.default.gc_thresh1/d' \
+-e '/net.ipv4.neigh.default.gc_thresh2/d' \
+-e '/net.ipv4.neigh.default.gc_thresh3/d' \
+-e '/net.ipv4.neigh.default.gc_stale_time/d' \
+-e '/net.ipv4.conf.default.arp_announce/d' \
+-e '/net.ipv4.conf.lo.arp_announce/d' \
+-e '/net.ipv4.conf.all.arp_announce/d' \
+-e '/kernel.panic/d' \
+-e '/vm.dirty_ratio/d' \
+-e '/^#/d' \
+-e '/^$/d' \
 "$SYS_PATH"
 cat <<EOF >> "$SYS_PATH"
 fs.file-max = 67108864
@@ -1208,20 +1220,20 @@ echo
 echo -e "${YELLOW}Optimizing System Limits...${NC}"
 echo
 sleep 0.5
-sed -i '''/ulimit -c/d''' $PROF_PATH
-sed -i '''/ulimit -d/d''' $PROF_PATH
-sed -i '''/ulimit -f/d''' $PROF_PATH
-sed -i '''/ulimit -i/d''' $PROF_PATH
-sed -i '''/ulimit -l/d''' $PROF_PATH
-sed -i '''/ulimit -m/d''' $PROF_PATH
-sed -i '''/ulimit -n/d''' $PROF_PATH
-sed -i '''/ulimit -q/d''' $PROF_PATH
-sed -i '''/ulimit -s/d''' $PROF_PATH
-sed -i '''/ulimit -t/d''' $PROF_PATH
-sed -i '''/ulimit -u/d''' $PROF_PATH
-sed -i '''/ulimit -v/d''' $PROF_PATH
-sed -i '''/ulimit -x/d''' $PROF_PATH
-sed -i '''/ulimit -s/d''' $PROF_PATH
+sed -i '/ulimit -c/d' $PROF_PATH
+sed -i '/ulimit -d/d' $PROF_PATH
+sed -i '/ulimit -f/d' $PROF_PATH
+sed -i '/ulimit -i/d' $PROF_PATH
+sed -i '/ulimit -l/d' $PROF_PATH
+sed -i '/ulimit -m/d' $PROF_PATH
+sed -i '/ulimit -n/d' $PROF_PATH
+sed -i '/ulimit -q/d' $PROF_PATH
+sed -i '/ulimit -s/d' $PROF_PATH
+sed -i '/ulimit -t/d' $PROF_PATH
+sed -i '/ulimit -u/d' $PROF_PATH
+sed -i '/ulimit -v/d' $PROF_PATH
+sed -i '/ulimit -x/d' $PROF_PATH
+sed -i '/ulimit -s/d' $PROF_PATH
 echo "ulimit -c unlimited" | tee -a $PROF_PATH
 echo "ulimit -d unlimited" | tee -a $PROF_PATH
 echo "ulimit -f unlimited" | tee -a $PROF_PATH
@@ -1263,7 +1275,7 @@ read -p "Press Enter to continue..."
 check_core_version() {
 local url=$1
 local tmp_file=$(mktemp)
-curl -4 --max-time 1 -s -o "$tmp_file" "$url"
+curl --max-time 1 -s -o "$tmp_file" "$url"
 if [ $? -ne 0 ]; then
 colorize red "Failed to check latest core version"
 return 1
@@ -1278,7 +1290,7 @@ rm "$tmp_file"
 check_script_version() {
 local url=$1
 local tmp_file=$(mktemp)
-curl -4 --max-time 1 -s -o "$tmp_file" "$url"
+curl --max-time 1 -s -o "$tmp_file" "$url"
 if [ $? -ne 0 ]; then
 colorize red "Failed to check latest script version"
 return 1
@@ -1306,11 +1318,11 @@ fi
 else
 echo -e "${YELLOW}$Soolakh_SCRIPT does not exist in $DEST_DIR. No need to remove.${NC}"
 fi
-curl -4 -s -L -o "$DEST_DIR/$Soolakh_SCRIPT" "$SCRIPT_URL"
+curl -s -L -o "$DEST_DIR/$Soolakh_SCRIPT" "$SCRIPT_URL"
 echo
 if [ $? -eq 0 ]; then
 chmod +x "$DEST_DIR/$Soolakh_SCRIPT"
-colorize yellow "Type '''soolakh''' to run the script.\n" bold
+colorize yellow "Type 'soolakh' to run the script.\n" bold
 colorize yellow "For removing script type: rm -rf /usr/bin/soolakh\n" bold
 press_key
 exit 0
@@ -1320,12 +1332,12 @@ sleep 1
 return 1
 fi
 }
-RED='''\033[0;31m'''
-GREEN='''\033[0;32m'''
-YELLOW='''\033[0;33m'''
-CYAN='''\e[36m'''
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+CYAN='\e[36m'
 MAGENTA="\e[95m"
-NC='''\033[0m''' # No Color
+NC='\033[0m' # No Color
 display_menu() {
 clear
 display_logo
